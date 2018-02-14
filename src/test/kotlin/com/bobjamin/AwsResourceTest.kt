a5d3df9ee2416433c883bce67aed6487f3b3746e
Chris Barbour <xbobjamin@gmail.com>
2018-02-14 18:33:44 +0000

    Fixing Arn parsing to enable more than 6 ':' characters, closes #7

--- src/test/kotlin/com/bobjamin/AwsResourceTest.kt
+++ src/test/kotlin/com/bobjamin/AwsResourceTest.kt
@@ -15,4 +15,16 @@ class AwsResourceTest{
         assertEquals("instance", arn.subType)
         assertEquals("instance-id", arn.subId)
     }
+
+    @Test
+    fun `should match Log Group ARN`(){
+        val arn = AwsResource.Arn.from("arn:aws:logs:us-east-1:123456789012:log-group:my-log-group*:log-stream:my-log-stream*")
+        assertEquals("aws", arn.partition)
+        assertEquals("logs", arn.service)
+        assertEquals("us-east-1", arn.region)
+        assertEquals("123456789012", arn.account)
+        assertEquals("log-group:my-log-group*:log-stream:my-log-stream*", arn.resource)
+        assertEquals("log-group", arn.subType)
+        assertEquals("my-log-group*:log-stream:my-log-stream*", arn.subId)
+    }
 }
