1608bfa80cf8788adcdb1f539414084bb7c00e6f
Chris Barbour <xbobjamin@gmail.com>
2018-02-14 19:50:58 +0000

    Adding KMS Resource Finder, closes #9

--- src/test/kotlin/com/bobjamin/AwsFinderTest.kt
+++ src/test/kotlin/com/bobjamin/AwsFinderTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 class AwsFinderTest{
     @Test
     fun test(){
-        AwsResourceFinderIAM().findIn("--", listOf("us-east-1"))
+        AwsResourceFinderKMS().findIn("--", listOf("us-east-1"))
                 .flatMap { resource -> resource.relatedArns.map { resource.resource.arn.arn() + " -> " + it.arn() } }
                 .forEach{System.out.println(it)}
     }
---
+++ src/test/kotlin/com/bobjamin/AwsResourceFinderKMSTest.kt
@@ -0,0 +1,30 @@
+package com.bobjamin
+
+import com.amazonaws.services.kms.AWSKMS
+import com.amazonaws.services.kms.model.KeyListEntry
+import com.amazonaws.services.kms.model.ListKeysResult
+import com.amazonaws.services.lambda.AWSLambda
+import com.amazonaws.services.lambda.model.FunctionConfiguration
+import com.amazonaws.services.lambda.model.ListFunctionsResult
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.mockito.Mockito
+
+class AwsResourceFinderKMSTest {
+
+    @Test
+    fun `should build appropriate relationships for kms resources`(){
+        val kmsClient = Mockito.mock(AWSKMS::class.java)
+        val listKeysResult = ListKeysResult().withKeys(listOf(
+                KeyListEntry().withKeyArn(AwsResource.Arn.from(AwsResourceType.KEY,"r", "a", "key-id").arn())
+        ))
+        Mockito.`when`(kmsClient.listKeys(Mockito.any())).thenReturn(listKeysResult)
+        val resources = AwsResourceFinderKMS({ kmsClient }).findIn("a", listOf("us-east-1"))
+        assertEquals(1, resources.size)
+        with(resources[0]){
+            with(this.resource){
+                assertEquals("arn:aws:kms:r:a:key/key-id", this.arn.arn())
+            }
+        }
+    }
+}
