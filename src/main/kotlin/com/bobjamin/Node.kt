9d146f06dbaa55013e57116fbcfca4fd4622568b
Chris Barbour <xbobjamin@gmail.com>
2018-02-20 22:06:29 +0000

    added initial group function

--- src/main/kotlin/com/bobjamin/Node.kt
+++ src/main/kotlin/com/bobjamin/Node.kt
@@ -2,7 +2,7 @@ package com.bobjamin
 
 import java.util.*
 
-data class NodeTree(val nodes: List<Node>, val links: List<NodeLink>)
+data class NodeTree(val nodes: List<Node> = emptyList(), val links: List<NodeLink> = emptyList())
 data class Node(val name: String, val info: AwsResource.Info, val uuid: String = UUID.randomUUID().toString())
 data class NodeLink(val uuidA: String, val uuidB: String, val direction: LinkDirection)
 enum class LinkDirection{ FORWARD, BACKWARD, BOTH }
@@ -15,4 +15,25 @@ object NodeBuilder{
         }.flatMap { node -> node.second.map { NodeLink(node.first.uuid, it.uuid, LinkDirection.FORWARD) } }
         return NodeTree(nodes.map { it.first }, links)
     }
+
+    fun groupFrom(node: Node, nodeTree: NodeTree): NodeTree{
+        return if(nodeTree.nodes.isEmpty() ||nodeTree.links.isEmpty())NodeTree()
+        else{
+            val directLinks = nodeTree.links.filter { it.uuidA == node.uuid || it.uuidB == node.uuid }
+            val linkedNodes = directLinks.mapNotNull { link -> nodeTree.nodes.find { it.uuid == (if(link.uuidA == node.uuid) link.uuidB else link.uuidA)} }
+            if(directLinks.isEmpty() || linkedNodes.isEmpty()) NodeTree()
+            else{
+                linkedNodes.fold(Pair(NodeTree(),NodeTree(nodeTree.nodes.subtract(linkedNodes + node), nodeTree.links.subtract(directLinks)))){
+                    pair, relatedNode ->
+                    val nextGroup = groupFrom(relatedNode,pair.second)
+                    Pair(
+                            NodeTree(pair.first.nodes + nextGroup.nodes, pair.first.links + nextGroup.links),
+                            NodeTree(pair.second.nodes.subtract(nextGroup.nodes), pair.second.links.subtract(nextGroup.links))
+                    )
+                }.first
+            }
+        }
+    }
 }
+
+fun <E: Any> List<E>.subtract(other: List<E>): List<E> = this.filterNot { other.contains(it) }
